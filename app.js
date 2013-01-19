9bb15b786ad97d5040a081aee6fe5d66ede307b6
Alejandro Morales <vamg008@gmail.com>
2013-01-18 22:29:34 -0600

    [fix] Refactor routes

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ var express  = require('express')
   , blogp = require('poet')
   , app    = express()
   , poet   = blogp(app)
+  , routes
 
+/* poet configuration */
 poet.set({
   metaFormat:'json',
   readMoreLink : function (post){
     var anchor = '<a href="' + post.url + '" title="Leer más de ' + post.title + '">Leer más</a>'
     return '<p>' + anchor + '</p>'
   },
+  routes: {
+    category: '/s/',
+    post: '/p/',
+    tag: '/t/',
+    page: '/pg/'
+  }
 }).init()
 
 { /* express configuration */
@@ -20,50 +28,13 @@ poet.set({
   app.use(app.router)
 }
 
+router = require('./routes')(app)
 
-app.get('/post/:post', function (req, res){
-  var post = req.poet.getPost(req.params.post)
-  if ( post ) {
-    res.render('post', { post: post })
-  } else {
-    res.send(404)
-  }
-})
-
-app.get('/tag/:tag', function (req, res){
-  var taggedPosts = req.poet.postsWithTag( req.params.tag );
-  if ( taggedPosts.length ) {
-    res.render( 'tag', {
-      posts : taggedPosts,
-      tag : req.params.tag
-    })
-  }
-})
-
-app.get('/category/:category', function (req, res){
-  var categorizedPosts = req.poet.postsWithCategory(req.params.category)
-  if ( categorizedPosts.length ) {
-    res.render( 'category', {
-      posts : categorizedPosts,
-      category : req.params.category
-    })
-  }
-})
-
-app.get('/page/:page', function (req, res){
-  var page = req.params.page
-    , lastPost = page * 3
-  res.render( 'page', {
-    posts : req.poet.getPosts( lastPost - 3, lastPost ),
-    page : page
-  })
-})
-
-
-
-app.get('/', function (req, res){
-  res.render('index')
-})
+app.get('/', router.home)
+app.get('/p/:post', router.post)
+app.get('/t/:tag', router.tag)
+app.get('/s/:category', router.series)
+app.get('/pg/:page', router.pages)
 
 app.listen(app.get('port'), function (){
   console.log('[*] Listening on %d', app.get('port'))
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,49 @@
 
 /*
- * GET home page.
+ * Todas las rutas
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+module.exports = function (app){
+
+  var Routes = {
+    home: function (req, res){
+      res.render('index')
+    },
+    post: function (req, res){
+      var post = req.poet.getPost(req.params.post)
+      if ( post ) {
+        res.render('post', { post: post })
+      } else {
+        res.send(404)
+      }
+    },
+    tag:  function (req, res){
+      var taggedPosts = req.poet.postsWithTag( req.params.tag );
+      if ( taggedPosts.length ) {
+        res.render( 'tag', {
+          posts : taggedPosts,
+          tag : req.params.tag
+        })
+      }
+    },
+    series: function (req, res){
+      var categorizedPosts = req.poet.postsWithCategory(req.params.category)
+      if ( categorizedPosts.length ) {
+        res.render( 'category', {
+          posts : categorizedPosts,
+          category : req.params.category
+        })
+      }
+    },
+    pages: function (req, res){
+      var page = req.params.page
+        , lastPost = page * 3
+      res.render( 'page', {
+        posts : req.poet.getPosts( lastPost - 3, lastPost ),
+        page : page
+      })
+    }
+  }
+
+  return Routes
+}
